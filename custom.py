239e16c5695c66c672006923ba7f064aad61688a
Aayush219 <abdbhartiya.1999@gmail.com>
2026-05-17 00:25:23 +1000

    Creating GUI

--- custom.py
+++ custom.py
@@ -1,13 +1,16 @@
-def get_todos(filepath="todos.txt"):
+FILEPATH = "todos.txt"
+ 
+ 
+def get_todos(file_path=FILEPATH):
     """ Read a text file and return the list of
     to-do items.
     """
-    with open(filepath, 'r') as file_local:
-        todos_local = file_local.readlines()
+    with open(file_path, "r") as file:
+        todos_local = file.readlines()
     return todos_local
  
  
-def write_todos(todos_arg, filepath="todos.txt"):
-    """ Write the to-do items list in the text file."""
-    with open(filepath, 'w') as file:
+def write_todos(todos_arg, file_path=FILEPATH):
+    """ Write the to-do items list to a text file. """
+    with open(file_path, "w") as file:
         file.writelines(todos_arg)
