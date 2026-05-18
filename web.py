b74a664fa2421a2853c6777ccf32d6fc14745464
Aayush219 <abdbhartiya.1999@gmail.com>
2026-05-18 20:35:28 +1000

    Adding changes to complete

--- web.py
+++ web.py
@@ -12,8 +12,15 @@ def add_todo():
 st.title("My Todo App")
 st.subheader("This is my todo app.")
 
-for todo in todos:
-    st.checkbox(todo)
+for index, todo in enumerate(todos):
+    checkbox = st.checkbox(todo, key=todo)
+    if checkbox:
+        todos.pop(index)
+        custom.write_todos(todos)
+        del st.session_state[todo]
+        st.rerun()
 
 st.text_input(label="",placeholder="Add new todo...", 
               on_change=add_todo, key='new_todo')
+
+
